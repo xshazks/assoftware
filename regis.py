71ea4884c0e8b65a38ab94d63dbc654896149f51
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-05-08 15:37:08 +0700

    tes br

--- regis.py
+++ regis.py
@@ -24,14 +24,14 @@ time.sleep(2)
 
 tombol = driver.find_element(By.CLASS_NAME,"TRX6J.CxLjL.qjTo7.CguuB.yC5pj")
 tombol.click()
+time.sleep(2)
 
-driver.find_element(By.NAME,"month")
-driver.find_element(By.NAME,"day").send_keys("1")
-driver.find_element(By.NAME,"year").send_keys("2003")
-
-
-tombol = driver.find_element(By.CLASS_NAME,"TRX6J.CxLjL.qjTo7.CguuB.yC5pj.E_Qq0")
+tombol = driver.find_element(By.CLASS_NAME,"Z8Ux2.qjTo7.IMvK3")
 tombol.click()
 
-# tombol = driver.find_element_by_id ("month")
-# tombol.select_by_index (1)
+driver.find_element(By.NAME,"email").send_keys("shahiezasauki@gmail.com")
+driver.find_element(By.NAME,"password").send_keys("Uki092515")
+time.sleep(2)
+
+tombol = driver.find_element(By.CLASS_NAME,"TRX6J.CxLjL.qjTo7.CguuB.yC5pj")
+tombol.click()
