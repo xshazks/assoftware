066efcd0945c3c357b139fae14e127f2b5471127
Shahieza Alfadia Shauqie <shahiezasauki15@gmail.com>
2024-05-08 15:46:53 +0700

    fixdeh

--- regis.py
+++ regis.py
@@ -18,7 +18,7 @@ time.sleep(2)
 driver.get("https://www.tumblr.com/register")
 
 
-driver.find_element(By.NAME,"email").send_keys("shahiezasauki@gmail.com")
+driver.find_element(By.NAME,"email").send_keys("shahiezasauki05@gmail.com")
 driver.find_element(By.NAME,"password").send_keys("Uki092515")
 time.sleep(2)
 
@@ -29,7 +29,7 @@ time.sleep(2)
 tombol = driver.find_element(By.CLASS_NAME,"Z8Ux2.qjTo7.IMvK3")
 tombol.click()
 
-driver.find_element(By.NAME,"email").send_keys("shahiezasauki@gmail.com")
+driver.find_element(By.NAME,"email").send_keys("shahiezasauki05@gmail.com")
 driver.find_element(By.NAME,"password").send_keys("Uki092515")
 time.sleep(2)
 
